d2d6aaf4c80f8e89b42ea7072c89acfca1997c2d
eloylozano <lozanobarrioseloy@gmail.com>
2025-02-08 18:37:27 +0100

    add product table

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,12 +1,50 @@
-import Layout from '@/components/Layout'
+import Layout from '@/components/Layout';
+import axios from 'axios';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
+// Definir la interfaz para los productos
+interface Product {
+  _id: string;
+  title: string;
+  description: string;
+  price: number;
+}
 
 export default function Products() {
+    const [products, setProducts] = useState<Product[]>([]); // Usar la interfaz Product
 
-    return(
+    useEffect(() => {
+        axios.get('/api/products').then(response => {
+            setProducts(response.data);
+        });
+    }, []);
+
+    return (
         <Layout>
-            <Link className='bg-blue-900 rounded-md text-white  py-1 px-2' href={'/products/new'}>Add new product
+            <Link className='bg-blue-900 rounded-md text-white py-1 px-2' href={'/products/new'}>
+                Add new product
             </Link>
+            <table className='basic mt-2'>
+                <thead>
+                    <tr>
+                        <td>Product name</td>
+                        <td></td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map((product) => (
+                        <tr>
+                            <td>{product.title}</td>
+                            <td>
+                                <Link href={'/products/'+ product._id}>
+                                Edit
+                                </Link>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </Layout>
-    )
+    );
 }
--- pages/products/new.tsx
+++ pages/products/new.tsx
@@ -1,17 +1,26 @@
 import Layout from "@/components/Layout";
 import { useState } from "react";
 import axios from "axios";
+import { useRouter } from "next/router";
 
 export default function NewProduct() {
+
   const [title, setTitle] = useState("");
   const [description, setdDescription] = useState("");
   const [price, setPrice] = useState("");
-  async function createProduct(ev){
+  const [goToProducts, setGoToProducts] = useState(false);
+  const router = useRouter();
+  
+  async function createProduct(ev: React.FormEvent<HTMLFormElement>){
     ev.preventDefault();
     const data = {title, description, price}
     await axios.post('/api/products', data)
+    setGoToProducts(true);
   }
 
+  if(goToProducts) {
+    router.push('/products')
+  }
 
   return (
     <Layout>
