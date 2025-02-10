d45a0d9da088006fe41f3b2451be9393b94bf941
eloylozano <lozanobarrioseloy@gmail.com>
2025-02-10 09:59:38 +0100

    fix errors from merge

--- models/Product.tsx
+++ models/Product.tsx
@@ -1,7 +1,6 @@
 
 import mongoose, {model, Schema, models} from "mongoose";
 
-import mongoose, {model, Schema, models} from "mongoose";
 const ProductSchema = new Schema({
   title: {type:String, required:true},
   description: String,
